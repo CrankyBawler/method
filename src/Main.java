398a449d47a49d9cdb1e5303b0f1eb257dd5badb
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-22 17:48:26 +0300

    Задача №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static int year(int year) {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + "год — невисокосный год");
+        }
+        return year;
     }
-}
+
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int year = 2008;
+        year(year);
+        }
+    }
